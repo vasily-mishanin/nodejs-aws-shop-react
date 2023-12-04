c9c2cbfc86781b5d95b13284f56d9e768dece218
vasily <vasilymishanin@gmail.com>
2023-12-04 23:39:57 +0600

    feat: CSV file validation

--- webApp/src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ webApp/src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -2,20 +2,57 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import axios from 'axios';
+import CSVFileValidator from 'csv-file-validator';
 
 type CSVFileImportProps = {
   url: string;
   title: string;
 };
 
+const validatorConfig = {
+  headers: [
+    { name: 'id', inputName: 'id' },
+    { name: 'title', inputName: 'title' },
+    { name: 'description', inputName: 'description' },
+    { name: 'price', inputName: 'price' },
+    { name: 'discountPercentage', inputName: 'discountPercentage' },
+    { name: 'rating', inputName: 'rating' },
+    { name: 'stock', inputName: 'stock' },
+    { name: 'brand', inputName: 'brand' },
+    { name: 'category', inputName: 'category' },
+    { name: 'thumbnail', inputName: 'thumbnail' },
+  ], // required
+  isHeaderNameOptional: true,
+  isColumnIndexAlphabetic: false,
+};
+
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File>();
+  const [isInvalidFile, setIsInvalidFile] = React.useState(false);
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files.length > 0) {
       const file = files[0];
-      setFile(file);
+
+      if (!file.name.toLowerCase().endsWith('.csv')) {
+        setFile(undefined);
+        setIsInvalidFile(true);
+      }
+
+      CSVFileValidator(file, validatorConfig)
+        .then((csvData) => {
+          if (csvData.inValidData.length > 0) {
+            setFile(undefined);
+            setIsInvalidFile(true);
+          } else {
+            setFile(file);
+            setIsInvalidFile(false);
+          }
+        })
+        .catch((err) => {
+          console.error('Error while validating CSV', err);
+        });
     }
   };
 
@@ -56,11 +93,17 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
         {title}
       </Typography>
       {!file ? (
-        <input type='file' onChange={onFileChange} />
+        <>
+          <input type='file' onChange={onFileChange} />
+          {isInvalidFile && (
+            <span>Not valid CSV. Please choose valid CSV file</span>
+          )}
+        </>
       ) : (
         <div>
           <button onClick={removeFile}>Remove file</button>
           <button onClick={uploadFile}>Upload file</button>
+          {!isInvalidFile && <span>{file.name}</span>}
         </div>
       )}
     </Box>
