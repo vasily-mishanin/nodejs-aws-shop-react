cfd734f5235d1072827204906446c02679b77236
vasily <vasilymishanin@gmail.com>
2024-01-02 11:43:27 +0600

    feat: AWS EBS endpoint for cart api

--- webApp/src/constants/apiPaths.ts
+++ webApp/src/constants/apiPaths.ts
@@ -6,7 +6,13 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://xx44v5lsg3.execute-api.us-east-1.amazonaws.com/prod', // call lambda to get url for uploading a file with ?name=<fileName>
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  //cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  //cart: 'https://1pixe3aon6.execute-api.us-east-1.amazonaws.com/prod/api', // cart serivce - Nest JS as Lambda
+  //https://8n6zt7471d.execute-api.us-east-1.amazonaws.com/api/profile/cart
+
+  // AWS Elastic Beanstalk domain
+  // http://vasily-mishanin-cart-api-test.us-east-1.elasticbeanstalk.com/api
+  cart: 'https://8n6zt7471d.execute-api.us-east-1.amazonaws.com/api', // AWS Elastic Beanstalk domain proxy with API Gateway
 };
 
 export default API_PATHS;
