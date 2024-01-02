69520672a97c149e605ff923d3fbada731cb36e9
vasily <vasilymishanin@gmail.com>
2024-01-02 12:08:05 +0600

    fix

--- webApp/src/constants/apiPaths.ts
+++ webApp/src/constants/apiPaths.ts
@@ -7,12 +7,11 @@ const API_PATHS = {
   import: 'https://xx44v5lsg3.execute-api.us-east-1.amazonaws.com/prod', // call lambda to get url for uploading a file with ?name=<fileName>
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   //cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  //cart: 'https://1pixe3aon6.execute-api.us-east-1.amazonaws.com/prod/api', // cart serivce - Nest JS as Lambda
-  //https://8n6zt7471d.execute-api.us-east-1.amazonaws.com/api/profile/cart
+  cart: 'https://1pixe3aon6.execute-api.us-east-1.amazonaws.com/prod/api', // cart serivce - Nest JS as Lambda
 
   // AWS Elastic Beanstalk domain
   // http://vasily-mishanin-cart-api-test.us-east-1.elasticbeanstalk.com/api
-  cart: 'https://8n6zt7471d.execute-api.us-east-1.amazonaws.com/api', // AWS Elastic Beanstalk domain proxy with API Gateway
+  //cart: 'https://8n6zt7471d.execute-api.us-east-1.amazonaws.com/api', // AWS Elastic Beanstalk domain proxy with API Gateway
 };
 
 export default API_PATHS;
